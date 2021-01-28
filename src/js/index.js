23062ce20bddda2cd680c40090e6587874ad9a75
Martins Akeredolu <olamideakeredolu@gmail.com>
2021-01-28 16:37:07 +0100

    minor

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './App'
-// import '../scss/bootstrap.min.css';
-import '../scss/style.scss'
-//entry point for the app, imported react and the styling and the app component before rendering to the DOM
-ReactDOM.render(<App />, document.getElementById("root"))
-=======
 import App from './App';
 import '../scss/style.scss';
 import BudgetContextProvider from './contexts/BudgetContext';
@@ -17,4 +10,3 @@ ReactDOM.render(
   </BudgetContextProvider>,
   document.getElementById('root')
 );
->>>>>>> react-version
